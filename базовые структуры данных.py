b4afb6354a1472495a7c942d7d55345b50afd60f
sumraks1 <shulgavladislav1337@gmail.com>
2024-08-05 22:40:24 +0700

    Add files via upload

---
+++ базовые структуры данных.py
@@ -0,0 +1,16 @@
+
+#1st program
+print(5*(9**0.5))
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+#4st program
+print(float('123.456'))
+print(float('123.456')* 10)
+print(int(float('123.456')/10/3))
